802caf0f3eab908e915a66b308c13147107398d3
Dennis Sandmark <d.sandmark@gmail.com>
2016-03-22 14:33:02 +0100

    No longer setting Numeral on window/global object

--- numeral.js
+++ numeral.js
@@ -662,14 +662,6 @@
         module.exports = numeral;
     }
 
-    /*global ender:false */
-    if (typeof ender === 'undefined') {
-        // here, `this` means `window` in the browser, or `global` on the server
-        // add `numeral` as a global object via a string identifier,
-        // for Closure Compiler 'advanced' mode
-        this['numeral'] = numeral;
-    }
-
     /*global define:false */
     if (typeof define === 'function' && define.amd) {
         define([], function () {
